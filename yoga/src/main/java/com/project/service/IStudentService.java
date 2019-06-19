ecbba59813e0c269f80f20b43397958a109dbeb5
wenran <a120935779@vip.qq.com>
2019-06-19 17:57:51 +0800

    我错了,login应该传String  --文然

--- yoga/src/main/java/com/project/service/IStudentService.java
+++ yoga/src/main/java/com/project/service/IStudentService.java
@@ -5,5 +5,5 @@ import com.project.bean.StudentBean;
 public interface IStudentService {
 	
 	public boolean regist(StudentBean student);
-	public boolean login (StudentBean student);
+	public boolean login (String name);
 }
