acd80e5fd479fe97f9f66b12a42c9962ea2b6a09
AlanYuChen <752998419@qq.com>
2019-06-19 16:35:14 +0800

    change IGymDao

--- yoga/src/main/java/com/project/dao/IGymDao.java
+++ yoga/src/main/java/com/project/dao/IGymDao.java
@@ -19,7 +19,7 @@ public interface IGymDao {
 	 * @param gym
 	 * @return
 	 */
-	@Insert("INSERT INTO t_gym(g_email,g_phone,g_qq) values(#{email},#{phone},#{qq})")
+	@Insert("INSERT INTO t_gym(g_email,g_phone,g_qq) values(#{g_email},#{g_phone},#{g_qq})")
 	int addGym(GymBean gym);
 
 	/**
@@ -27,7 +27,7 @@ public interface IGymDao {
 	 * 
 	 * @param id
 	 * @param pwd
-	 * @return
+	 * @return 影响行数
 	 */
 	@Update("UPDATE t_gym SET g_password=#{pwd} WHERE g_id=#{id}")
 	int updatePassowrd(@Param("id") int id, @Param("pwd") String pwd);
