809d4bbc97545659255aad3a363c77dadeef8a34
huangrenjie <1562503602@qq.com>
2019-06-19 19:57:38 +0800

    添加GymServiceImpl

--- yoga/src/main/java/com/project/service/IGymService.java
+++ yoga/src/main/java/com/project/service/IGymService.java
@@ -1,6 +1,11 @@
 package com.project.service;
 
+import java.util.List;
+
+import com.project.bean.CoachBean;
 import com.project.bean.GymBean;
+import com.project.bean.LessonBean;
+import com.project.bean.PictrueBean;
 
 /**
  * 场馆业务层接口
@@ -33,5 +38,56 @@ public interface IGymService {
 	 * @return 影响行数
 	 */
 	int updatePassword(String id, String pwd);
+	
+	
+	/**
+	 * 查找所有场馆
+	 * @return
+	 */
+	public List<GymBean> findAllGym();
+	
+	/**
+	 * 查找场馆by id
+	 * @param id
+	 * @return
+	 */
+	public GymBean findGymById(String id);
+	
+	/**
+	 * 信息完善
+	 * @param gymBean
+	 * @return
+	 */
+	public int updateMessage(GymBean gymBean);
+	
+	/**
+	 * 场馆添加图片
+	 * @param lists
+	 * @return
+	 */
+	public int addPictrue(List<PictrueBean> lists);
+	
+	/**
+	 * 教练课程安排
+	 * @param lessonBean
+	 * @return
+	 */
+	public int addLesson(LessonBean lessonBean);
+	
+	
+	/**
+	 * 查看我的签约教练
+	 * @param g_id
+	 * @return
+	 */
+	public List<CoachBean> findMyCoach(String g_id);
+	
+	/**
+	 * 解约签约教练
+	 * @param coachBean
+	 * @return
+	 */
+	public int endDeal(CoachBean coachBean);
+	
 
 }
