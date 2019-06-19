38084066d78789a804b34d462b38ce7e187e9a23
visitor <wenpanss@163.com>
2019-06-19 18:08:52 +0800

    realm更新

--- yoga/src/main/java/com/project/realm/MyRealm.java
+++ yoga/src/main/java/com/project/realm/MyRealm.java
@@ -14,6 +14,7 @@ import com.project.bean.GymBean;
 import com.project.bean.StudentBean;
 import com.project.service.ICoachService;
 import com.project.service.IGymService;
+import com.project.service.IStudentService;
 
 /**
  * 学员安全数据源
