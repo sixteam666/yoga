991ef80f5d7c588631c11029f2d120ebb1222ca6
AlanYuChen <752998419@qq.com>
2019-06-19 10:51:32 +0800

    test

--- yoga/src/main/java/com/project/controller/ControllerTest.java
+++ yoga/src/main/java/com/project/controller/ControllerTest.java
@@ -1,5 +1,8 @@
 package com.project.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ControllerTest {
 	public static void main(String[] args) {
 		System.out.println("this is controller test!!!");
