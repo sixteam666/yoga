ddcda6db541a602e199435dbee7720163c08dd0a
mygit <771767933@qq.com>
2019-06-20 10:16:38 +0800

    *

--- yoga/src/main/java/com/project/dao/ICoachDao.java
+++ yoga/src/main/java/com/project/dao/ICoachDao.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.One;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
