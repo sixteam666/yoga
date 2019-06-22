7c5e0fbdb3c2476a0a816b9dc6a90ac09fecac6b
AlanYuChen <752998419@qq.com>
2019-06-22 17:26:31 +0800

    update

--- yoga/src/main/java/com/project/controller/GymController.java
+++ yoga/src/main/java/com/project/controller/GymController.java
@@ -61,16 +61,16 @@ public class GymController {
 				// 调用login进行认证
 				currentUser.login(token);
 				System.out.println("认证成功");
-				return "redirect:/html/main.html";
+				return "redirect:/html/gym/gym.html";
 			}
 			// 父异常。认证失败异常
 			catch (AuthenticationException ae) {
 				// unexpected condition? error?
 				System.out.println("异常不详：自己解决");
-				return "redirect:/html/login.html";
+				return "redirect:/html/gym/gymLogin.html";
 			}
 		}
-		return "redirect:/html/main.html";
+		return "redirect:/html/.html";
 	}
 	
 	/**
@@ -86,24 +86,33 @@ public class GymController {
 		}
 		return true; 
 	}
-
+	
 	/**
 	 * 注册
 	 * 
-	 * @param gym
-	 * @return
+	 * @param regName 注册的登录名（邮箱或电话号）
+	 * @param g_password 注册的密码
+	 * @return 注册状态 1：注册成功，0：注册失败 -1：登录名格式错误
 	 */
 	@RequestMapping("/reg.do")
 	@ResponseBody
-	public int register(GymBean gym) {
+	public int register(String regName, String g_password) {
+		System.out.println("reg.do测试:GymBean:" + regName +"--" + g_password);
+		GymBean gym = new GymBean();
 		gym.setG_id(UUID.randomUUID().toString());
-
+		
+		if(regName.contains("@")) {
+			gym.setG_email(regName);
+		}else if(regName.length() == 11) {
+			gym.setG_phone(regName);
+		}else {
+			return -1; // 格式不符合要求
+		}
 		// 盐值暂时无法确定
 		/*
 		 * Object obj = new SimpleHash("MD5", gym.getG_password(),1024);
 		 * gym.setG_password(obj.toString());
 		 */
-
 		int result = gymService.register(gym);
 		return result;
 	}
